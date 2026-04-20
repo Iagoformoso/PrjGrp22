798e99eed7e92f8a75b1f97611acf765c3965648
Chopy5 <miguel.garcia0@rai.usc.es>
2026-04-20 16:51:19 +0200

    Main actualizado
    Función main funcional, para testear que clonar el repositorio e incluirlo en Eclipse funciona correctamente.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,13 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		int a = 0;
+		
+		a+=4;
+		
 		System.out.println("Hola");
 		
-		return 0;
+		System.out.println("Valor de a: " + a);
+		
 	}
 }
